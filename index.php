a202b24dbd4f53fab1e1328a6b4b6213c3923863
myfreelanceblog <myfreelanceblog@users.noreply.github.com>
2023-09-27 15:51:30 +0300

    Clear themplate HTML

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="format-detection" content="telephone=no">
+	<meta content="True" name="HandheldFriendly">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" name="viewport">
+
+	<title>Title</title>
+
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon.png">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
